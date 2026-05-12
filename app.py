4f1ae32a9f466660a156e235a922cd1c2c79c73e
Robosword <32asingh@stratfordschools.net>
2026-05-12 14:07:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/")
+def sendMessage():
+  pass
+
+@app.route("/addUser")
+def addUser():
+  pass
