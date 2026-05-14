a207e8f0be45883b1cb05263673afff85e3b5a05
Robosword <32asingh@stratfordschools.net>
2026-05-13 21:15:47 -0700

    Fix some errors, and stuff like that

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template, jsonify, request
 from dataManagment import *
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/sendMessage")
 def sendMessage():
-  pass
+    pass
 
-@app.route("/getChat", methods=["GET", "POST"])
+
 @app.route('/getChat', methods=['POST'])
 def get_chat():
-
     data = request.get_json()
 
     username = data.get("username")
@@ -25,5 +27,7 @@ def get_chat():
         "error": a,
         "messages": a
     })
+
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
